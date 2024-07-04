900af04ebe0a5bc303caf2325b6830ecd04e6d83
Kauã dos Reis de Oliveira <127523958+KauaoLiveira06@users.noreply.github.com>
2024-07-04 08:06:34 -0300

    main.js

--- main.js
+++ main.js
@@ -40,3 +40,19 @@ const perguntas = [
         ]
     }
 ]
+
+let atual = 0 
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraPergunta();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        caixaAlternativas.appendChild(botaoAlternativas)
+    }
+}
